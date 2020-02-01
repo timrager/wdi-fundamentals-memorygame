0565f49f6045012769e2b4a0abbc892f80eee959
tadhg <tadhg@macbook-pro.lan>
2020-02-01 10:30:02 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,24 +3,45 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-
-if (cardsInPlay.length === 2)
-{
+function checkForMatch(){
 
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry try again");
 	}
+}
+
+var flipcard = function(cardId){
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+}
+
+flipcard(0);
+flipcard(2);
+
+
+
+
+/* OLD UNUSED CODE HERE
+
+	var cardOne = cards[0];
+	var cardTwo = cards[1];
+
+	cardsInPlay.push(cardOne);
+	cardsInPlay.push(cardTwo);
+
+
+
+if (cardsInPlay.length === 2)
+{
+
+	
 
 console.log("User flipped " + cardsInPlay[0]);
 console.log("User flipped " + cardsInPlay[1]);
 
-}
+} */
